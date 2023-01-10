152f5014cb95d9a839172f2cf156df51c72925c3
Szymon Bąk <50375919+boonku@users.noreply.github.com>
2023-01-10 14:25:02 +0100

    add cors (#6)

--- backend/src/main/java/hospitech/HospitechSystemBackendApplication.java
+++ backend/src/main/java/hospitech/HospitechSystemBackendApplication.java
@@ -2,27 +2,10 @@ package hospitech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HospitechSystemBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(HospitechSystemBackendApplication.class, args);
-    }
-
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("OPTION", "GET", "PUT", "POST", "DELETE")
-                        .allowedOrigins("http://localhost:8080", "http://localhost:3000");
-            }
-        };
     }
 }
