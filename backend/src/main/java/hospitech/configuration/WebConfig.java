b25fe5878a5de6c239b8f30ac71e998dff29af84
Bodzisz <kacpiw01pol@gmail.com>
2023-01-28 18:18:20 +0100

    Add cors for prod env

--- backend/src/main/java/hospitech/configuration/WebConfig.java
+++ backend/src/main/java/hospitech/configuration/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:3000")
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "https://hospitech-5gx7z.ondigitalocean.app")
                 .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "OPTION", "PATCH");
     }
 }
